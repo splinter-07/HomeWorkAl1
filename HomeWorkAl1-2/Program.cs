a98ed68fac93cf8eb3aae23a0c43761b8c471e7f
splinter_07 <splinter_07@list.ru>
2021-02-23 02:07:26 +0300

    HomeWork

--- HomeWorkAl1-2/Program.cs
+++ HomeWorkAl1-2/Program.cs
@@ -10,6 +10,32 @@ namespace HomeWorkAl1_2
     {
         static void Main(string[] args)
         {
+
         }
+
+        public static int StrangeSum(int[] inputArray) // общая сложность - O(N^3)
+        {
+            int sum = 0;
+            for (int i = 0; i < inputArray.Length; i++) // O(N * N * N) 
+            {
+                for (int j = 0; j < inputArray.Length; j++) // O(N * N)
+                {
+                    for (int k = 0; k < inputArray.Length; k++) // O(N)
+                    {
+                        int y = 0; // O(1) константа
+
+                        if (j != 0) // O(1) константа
+                        {
+                            y = k / j;
+                        }
+
+                        sum += inputArray[i] + i + k + j + y; // O(1) константа
+                    }
+                }
+            }
+
+            return sum;
+        }
+
     }
 }
--- HomeWorkAl1/Program.cs
+++ HomeWorkAl1/Program.cs
@@ -8,8 +8,59 @@ namespace HomeWorkAl1
 {
     class Program
     {
+        class TestCase
+        {
+            public int InputA { get; set; }
+            public string Expected { get; set; }
+        }
         static void Main(string[] args)
         {
+            var testData = new TestCase[4];
+
+            testData[0] = new TestCase()
+            { InputA = 2, Expected = "Простое" };
+            testData[1] = new TestCase()
+            { InputA = 4, Expected = "Не простое" };
+            testData[2] = new TestCase()
+            { InputA = 11, Expected = "Простое" };
+            testData[3] = new TestCase()
+            { InputA = 15, Expected = "Не простое" };
+
+            foreach (var testCase in testData)
+            {
+                var result = NumberCheck(testCase.InputA);
+                var correct = result == testCase.Expected;
+                Console.WriteLine($"Число {testCase.InputA} - {result}, Expected {testCase.Expected}. Ответ - {correct}");
+            }
+            Console.ReadKey();
+        }
+
+        static string NumberCheck(int n)
+        {
+            int d = 0;
+            int i = 2;
+
+            while (i < n)
+            {
+                if (n % i == 0)
+                {
+                    d++;
+                    i++;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            if (d == 0)
+            {
+                return "Простое";
+            }
+            else
+            {
+                return "Не простое";
+            }
+
         }
     }
 }
